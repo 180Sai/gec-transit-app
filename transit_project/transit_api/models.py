9018bd39875f8c4af9ed466bb0cca50633eee591
jere-dev <jeremthom1@gmail.com>
2025-11-16 15:30:57 -0500

    tests

--- transit_project/transit_api/models.py
+++ transit_project/transit_api/models.py
@@ -1,45 +1,42 @@
-import uuid
-
 from django.db import models
-
-# Create your models here.
+from decimal import Decimal
 
 class Route(models.Model):
-	id = models.IntegerField(primary_key=True)
-	short_name = models.CharField(max_length=100)
-	long_name = models.CharField(max_length=100)
-	color = models.CharField(max_length=6)
+    id = models.IntegerField(primary_key=True)
+    short_name = models.CharField(max_length=100)
+    long_name = models.CharField(max_length=100)
+    color = models.CharField(max_length=6)
 
 class Stop(models.Model):
-	id = models.IntegerField()
-	code = models.IntegerField()
-	name = models.CharField(max_length=100)
-	desc = models.CharField(max_length=100)
-	latitude = models.FloatField(decimal_places=6)
-	longitude = models.FloatField(decimal_places=6)
+    id = models.IntegerField(primary_key=True)
+    code = models.IntegerField()
+    name = models.CharField(max_length=100)
+    desc = models.CharField(max_length=100)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
 
 class StopTime(models.Model):
-	trip_id = models.IntegerField()
-	arrival_time = models.TimeField()
-	departure_time = models.TimeField()
-	stop_id = models.IntegerField()
-	pick_up = models.BooleanField(default=True)
-	drop_off = models.BooleanField(default=True)
-	shape_dist_traveled = models.FloatField(decimal_places=5)
-	timepoint = models.BooleanField(default=True)
+    trip_id = models.IntegerField()
+    arrival_time = models.TimeField()
+    departure_time = models.TimeField()
+    stop_id = models.IntegerField()
+    pick_up = models.BooleanField(default=True)
+    drop_off = models.BooleanField(default=True)
+    shape_dist_traveled = models.DecimalField(max_digits=7, decimal_places=5)
+    timepoint = models.BooleanField(default=True)
 
 class Trip(models.Model):
-	route_id = models.IntegerField()
-	id = models.IntegerField()
-	trip_headsign = models.CharField(max_length=50)
-	direction_id = models.BooleanField(default=True)
-	shape_id = models.IntegerField()
-	is_accessible = models.BooleanField(default=True)
-	is_bikes = models.BooleanField(default=True)
+    id = models.IntegerField(primary_key=True)
+    route_id = models.IntegerField()
+    trip_headsign = models.CharField(max_length=50)
+    direction_id = models.BooleanField(default=True)
+    shape_id = models.IntegerField()
+    is_accessible = models.BooleanField(default=True)
+    is_bikes = models.BooleanField(default=True)
 
 class Shape(models.Model):
-	id = models.IntegerField()
-	shape_pt_lat = models.FloatField(decimal_places=5)
-	shape_pt_lon = models.FloatField(decimal_places=5)
-	shape_pt_sequence = models.IntegerField()
-	shape_dist_traveled = models.FloatField(decimal_places=4)
+    id = models.IntegerField(primary_key=True)
+    shape_pt_lat = models.DecimalField(max_digits=8, decimal_places=5)
+    shape_pt_lon = models.DecimalField(max_digits=8, decimal_places=5)
+    shape_pt_sequence = models.IntegerField()
+    shape_dist_traveled = models.DecimalField(max_digits=6, decimal_places=4)
