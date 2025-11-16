413b922fd955a800b0bae8b7ecf94144f03456e5
180Sai <92890058+180Sai@users.noreply.github.com>
2025-11-16 13:35:36 -0500

    add basic URL support for database

---
+++ transit_project/transit_api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import *
+
+router = DefaultRouter()
+router.register(r'routes', RouteViewSet)
+router.register(r'stops', StopViewSet)
+router.register(r'stoptimes', StopTimeViewSet)
+router.register(r'trips', TripViewSet)
+
+urlpatterns = [
+	path('', include(router.urls))
+]
