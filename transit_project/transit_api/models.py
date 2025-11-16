e3c62af391996728f66618863a64348316c66898
180Sai <92890058+180Sai@users.noreply.github.com>
2025-11-16 12:34:11 -0500

    add models

--- transit_project/transit_api/models.py
+++ transit_project/transit_api/models.py
@@ -1,3 +1,45 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+
+class Route(models.Model):
+	id = models.IntegerField(primary_key=True)
+	short_name = models.CharField(max_length=100)
+	long_name = models.CharField(max_length=100)
+	color = models.CharField(max_length=6)
+
+class Stop(models.Model):
+	id = models.IntegerField()
+	code = models.IntegerField()
+	name = models.CharField(max_length=100)
+	desc = models.CharField(max_length=100)
+	latitude = models.FloatField(decimal_places=6)
+	longitude = models.FloatField(decimal_places=6)
+
+class StopTimes(models.Model):
+	trip_id = models.IntegerField()
+	arrival_time = models.TimeField()
+	departure_time = models.TimeField()
+	stop_id = models.IntegerField()
+	pick_up = models.BooleanField(default=True)
+	drop_off = models.BooleanField(default=True)
+	shape_dist_traveled = models.FloatField(decimal_places=5)
+	is_exact = models.BooleanField(default=True)
+
+class Trip(models.Model):
+	route_id = models.IntegerField()
+	id = models.IntegerField()
+	trip_headsign = models.CharField(max_length=50)
+	direction_id = models.BooleanField(default=True)
+	shape_id = models.IntegerField()
+	is_accessible = models.BooleanField(default=True)
+	is_bikes = models.BooleanField(default=True)
+
+class Shape(models.Model):
+	id = models.IntegerField()
+	shape_pt_lat = models.FloatField(decimal_places=5)
+	shape_pt_lon = models.FloatField(decimal_places=5)
+	shape_pt_sequence = models.IntegerField()
+	shape_dist_traveled = models.FloatField(decimal_places=4)
