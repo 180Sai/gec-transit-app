e771067ee3748f2162e2a818b487b32192a8a90f
180Sai <92890058+180Sai@users.noreply.github.com>
2025-11-16 13:36:11 -0500

    add basic batch views

--- transit_project/transit_api/views.py
+++ transit_project/transit_api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+
 
 # Create your views here.
+
+class RouteViewSet(viewsets.ReadOnlyModelViewSet):
+	queryset = Route.objects.all()
+
+class StopViewSet(viewsets.ReadOnlyModelViewSet):
+	queryset = Stop.objects.all()
+
+class TripViewSet(viewsets.ReadOnlyModelViewSet):
+	queryset = Trip.objects.all()
+
+class StopTimeViewSet(viewsets.ReadOnlyModelViewSet):
+	queryset = StopTime.objects.all()
+
+class ShapesViewSet(viewsets.ReadOnlyModelViewSet):
+	queryset = Shape.objects.all()
